4e6cb0b6209e055d3f849527fb6ffe77a9cc380c
TobsA <git@tobsa.wtf>
2024-06-16 22:59:39 +0200

    Switch to new msg format

--- src/tts.py
+++ src/tts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import itertools
 import json
 import os
 import platform
@@ -7,11 +8,12 @@ import signal
 import time
 import threading
 import queue
+import typing
 import uuid
 from json import JSONDecodeError
 from os.path import isfile
 from subprocess import Popen
-
+from dataclasses import dataclass, field
 
 import paho.mqtt.client as mqtt
 from gtts import gTTS
@@ -26,7 +28,17 @@ logger = logging.getLogger(__name__)
 connected = threading.Event()
 stopProgram = threading.Event()
 stopCurrent = threading.Event()
-audioQueue = queue.Queue()
+disabled = threading.Event()
+audioQueue = queue.PriorityQueue()
+
+audioTaskCounter = itertools.count()
+
+
+@dataclass(order=True)
+class AudioTask:
+    priority: int
+    taskNum: int
+    cmds: typing.Any = field(compare=False)
 
 
 def signalhandler(signum):
@@ -58,52 +70,59 @@ def on_message_cmd(mqtt_client, data_object, msg):
     config = Config()
     logger.debug(msg.payload.decode('utf-8'))
     try:
-        try:
-            data = json.loads(msg.payload.decode('utf-8'))
-            assert "cmd" in data
-        except (JSONDecodeError, AssertionError):
-            logger.warning("Error decoding command")
-            return
-        logger.info(f"Received command: {data['cmd']}")
-        if data['cmd'].lower() not in ["stop", "stopall", "sleep", "sound", "tts"]:
+        data = json.loads(msg.payload.decode('utf-8'))
+        assert "tasks" in data
+    except (JSONDecodeError, AssertionError):
+        logger.warning("Error decoding command")
+        return
+
+    if "priority" in data:
+        priority = data["priority"]
+    else:
+        priority = 50
+
+    if "gain" in data and "channel" in data:
+        logger.warning("gain and channel are mutually exclusive")
+        return
+    elif "gain" in data and len(data["gain"]) == 2:
+        gain = data["gain"]
+    elif "channel" in data:
+        if data['channel'].lower() == "left":
+            gain = [0, -100]
+        elif data["channel"].lower() == "right":
+            gain = [-100, 0]
+        else:
+            gain = [0, 0]
+    else:
+        gain = [0, 0]
+
+    audio_tasks = []
+    for task in data["tasks"]:
+        logger.info(f"Received command: {task['cmd']}")
+        if task['cmd'].lower() not in ["sleep", "sound", "tts"]:
             logger.warning("Unknown command!")
             return
 
-        if data['cmd'].lower() == "stop":
-            stopCurrent.set()
+        # if task['cmd'].lower() == "stop":
+        #     stopCurrent.set()
+        #
+        # elif task['cmd'].lower() == "stopall":
+        #     with audioQueue.mutex:
+        #         audioQueue.queue.clear()
+        #         audioQueue.all_tasks_done.notify_all()
+        #         audioQueue.unfinished_tasks = 0
+        #     stopCurrent.set()
 
-        elif data['cmd'].lower() == "stopall":
-            with audioQueue.mutex:
-                audioQueue.queue.clear()
-                audioQueue.all_tasks_done.notify_all()
-                audioQueue.unfinished_tasks = 0
-            stopCurrent.set()
-
-        elif data['cmd'].lower() == "sleep":
-            if "data" not in data or not str(data['data']).isnumeric():
+        elif task['cmd'].lower() == "sleep":
+            if "data" not in task or not str(task['data']).isnumeric():
                 logger.warning("Error decoding data")
                 return
-            audioQueue.put(("sleep", data['data'], None), False)
+            audio_tasks.append(("sleep", task['data']))
 
-        elif data['cmd'].lower() in ["sound", "tts"]:
-            if "data" not in data:
+        else:
+            if "data" not in task:
                 logger.warning("Error decoding data")
                 return
-
-            if "gain" in data and "channel" in data:
-                logger.warning("gain and channel are mutually exclusive")
-                return
-            elif "gain" in data and len(data["gain"]) == 2:
-                gain = data["gain"]
-            elif "channel" in data:
-                if data['channel'].lower() == "left":
-                    gain = [0, -100]
-                elif data["channel"].lower() == "right":
-                    gain = [-100, 0]
-                else:
-                    gain = [0, 0]
-            else:
-                gain = [0, 0]
 
             filename = f"{uuid.uuid4()}"
             logger.debug(f"Filename: {filename}")
@@ -111,25 +130,33 @@ def on_message_cmd(mqtt_client, data_object, msg):
                 if not isfile(os.path.join(config[CONF_AUDIO_SOUNDS_PATH], f'{data["data"]}.wav')):
                     logger.warning("File does not exist")
                     return
-                shutil.copyfile(os.path.join(config[CONF_AUDIO_SOUNDS_PATH], f'{data["data"]}.wav'), os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
-                audioQueue.put(("sound", filename, gain), False)
+                thread = threading.Thread(target=sound_worker, args=[filename, data['data'], gain])
+                thread.start()
+                audio_tasks.append(("file", (thread, filename)))
 
             else:
                 if "data" not in data:
                     logger.warning("Error decoding data")
                     return
-                thread = threading.Thread(target=tts_worker, args=[filename, data['data']])
+                thread = threading.Thread(target=tts_worker, args=[filename, data['data'], gain])
                 thread.start()
-                audioQueue.put(("tts", (thread, filename), gain), False)
-
-        logger.debug(f"Command {data['cmd']} completed")
-
+                audio_tasks.append(("file", (thread, filename)))
+    try:
+        audioQueue.put_nowait(AudioTask(priority, next(audioTaskCounter), audio_tasks))
     except queue.Full:
         logger.critical("Audio queue is full")
         return
 
 
-def tts_worker(filename, text):
+def sound_worker(filename, sound, gain):
+    config = Config()
+    shutil.copyfile(os.path.join(config[CONF_AUDIO_SOUNDS_PATH], f'{sound}.wav'),
+                    os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
+
+    gain(filename, gain)
+
+
+def tts_worker(filename, text, gain):
     config = Config()
     logger.debug("Requesting TTS from Google")
     tts = gTTS(text, lang=config[CONF_TTS_LANG], tld=config[CONF_TTS_TLD], slow=config[CONF_TTS_SLOW])
@@ -144,63 +171,64 @@ def tts_worker(filename, text):
         os.remove(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.mp3'))
 
     logger.debug("Requesting TTS from Google completed")
+    gain(filename, gain)
+
+
+def gain(filename, gain):
+    if gain != [0, 0]:
+        config = Config()
+        logger.debug(f"Modifying gain to {gain}")
+        sound = AudioSegment.from_wav(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
+        sound = sound.apply_gain_stereo(*gain)
+        sound.export(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'), format="wav")
 
 
 def audio_worker():
     config = Config()
 
     while not stopProgram.is_set():
+        while disabled.is_set():
+            time.sleep(0.1)
         try:
             data = audioQueue.get_nowait()
-            cmd = data[0]
-            logger.debug("Running command: %s", cmd)
-            if cmd == "sleep":
-                logger.debug(f"Sleeping for {data[1]} seconds")
-                time.sleep(data[1])
-                continue
+            for cmd in data.cmds:
+                logger.debug(f"Running command: {cmd[0]}")
+                if cmd[0] == "sleep":
+                    logger.debug(f"Sleeping for {cmd[1]} seconds")
+                    time.sleep(cmd[1])
+                    continue
 
-            elif cmd in ["sound", "tts"]:
-                if cmd == "tts":
-                    t = data[1][0]
-                    filename = data[1][1]
+                elif cmd[0] in ["sound", "tts"]:
+                    t = cmd[1][0]
+                    filename = cmd[1][1]
                     if t.is_alive():
                         t.join(5)
                         if t.is_alive():
-                            logger.warning("TTS thread runs too long, skipping")
+                            logger.warning("Thread runs too long, skipping")
                             continue
+                    logger.info(f"Playing {os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav')}")
+                    if not config[CONF_AUDIO_DISABLE]:
+                        args = ['/usr/bin/paplay', '-p', '-d', config[CONF_AUDIO_DEVICE],
+                                os.path.abspath(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))]
+                        p = Popen(args)
+                        while p.poll() is None:
+                            if stopProgram.is_set() or stopCurrent.is_set():
+                                p.terminate()
+                                break
+                            time.sleep(0.1)
+                    else:
+                        sound = AudioSegment.from_wav(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
+                        logger.warning(
+                            f"Skipping playback as requested by config. Waiting for {len(sound) / 1000} seconds.")
+                        time.sleep(len(sound) / 1000)
+
+                    logger.debug("Playback complete.")
+                    if not config[CONF_AUDIO_KEEP_FILE]:
+                        logger.debug("Deleting audio file.")
+                        os.remove(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
                 else:
-                    filename = data[1]
-
-                # Modify gain?
-                if data[2] != [0, 0]:
-                    logger.debug(f"Modifying gain to {data[2]}")
-                    sound = AudioSegment.from_wav(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
-                    sound = sound.apply_gain_stereo(*data[2])
-                    sound.export(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}_edit.wav'), format="wav")
-                    os.remove(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
-                    filename += "_edit"
-
-                logger.info(f"Playing {os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav')}")
-                if not config[CONF_AUDIO_DISABLE]:
-                    args = ['/usr/bin/paplay', '-p', '-d', config[CONF_AUDIO_DEVICE], os.path.abspath(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))]
-                    p = Popen(args)
-                    while p.poll() is None:
-                        if stopProgram.is_set() or stopCurrent.is_set():
-                            p.terminate()
-                            continue
-                        time.sleep(0.1)
-                else:
-                    sound = AudioSegment.from_wav(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
-                    logger.warning(f"Skipping playback as requested by config. Waiting for {len(sound) / 1000} seconds.")
-                    time.sleep(len(sound) / 1000)
-
-                logger.debug("Playback complete.")
-                if not config[CONF_AUDIO_KEEP_FILE]:
-                    logger.debug("Deleting audio file.")
-                    os.remove(os.path.join(config[CONF_AUDIO_TEMP_PATH], f'{filename}.wav'))
-
-            else:
-                logger.critical("Unknown command")
+                    logger.critical("Unknown command")
+                stopCurrent.clear()
         except queue.Empty:
             time.sleep(1)
         except KeyboardInterrupt as e:
